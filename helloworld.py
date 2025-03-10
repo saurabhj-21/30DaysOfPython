bc9e3c15abe51b9809e05629ae1da9c01db62514
Saurabh Joardar <saurabh.iot.bdu@gmail.com>
2025-03-10 12:17:56 +0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+#day1
+print(2+3)
+print(3-1)
+print(2%3)
+print(3%2)
+print(3/2)
+print(3**2)
+print(3//2)
+
+#checking dara types
+print(type(10))                   #int
+print(type(2.12))                 #float
+print(type(1+3j))                 #complex
+print(type((3.22, 4.55, 2, 4.66)))  #touple
+print(type('Amin'))               #string
+print(type[1, 2, 3])              #list
+print(type({9.7, 5.6, 7.88}))     #set
+print(type({'Name':'Amin', 'Country':'Bangladesh'})) #dictionary
