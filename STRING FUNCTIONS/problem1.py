e72b4c7a2047ca4b533c0b668f64329661914c24
Saurabh Joardar <saurabh.iot.bdu@gmail.com>
2025-03-14 12:59:40 +0600

    Add files via upload

---
+++ STRING FUNCTIONS/problem1.py
@@ -0,0 +1,5 @@
+'''Write a python program to display a user entered 
+name followed by Good Afternoon using input () function.'''
+
+name = input("Say your name. " )
+print(f"Godd afternoon, {name}.")
---
+++ STRING FUNCTIONS/problem3.py
@@ -0,0 +1,8 @@
+'''Write a program to format the following letter 
+using escape sequence character'''
+
+#Dear John, I hope you are doing well. I wanted to inform you that we have a meeting tomorrow at 10 AM. Please be on time. Regards, Alex
+
+
+letter = "Dear John,\n\nI hope you are doing well.\nI wanted to inform you that we have a meeting tomorrow at 10 AM.\nPlease be on time.\n\nRegards,\nAlex"
+print(letter)
